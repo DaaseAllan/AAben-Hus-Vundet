eb38a37d5882e7bed4fc10af5775897bddeed688
DaaseAllan <laurits.mumberg@gmail.com>
2018-11-06 13:49:08 +0100

    Næsten playable ting

---
+++ AAbenHusSpil/Assets/BallMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    public float hSpeed;
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Wall check
+        if(transform.position.x >= 5.1)
+        {
+            //Skifter retning til venstre
+            rb.velocity = new Vector2(-hSpeed, 0);
+        }
+        else if(transform.position.x <= -5.1)
+        {
+            //Skifter retning til højre
+            rb.velocity = new Vector2(hSpeed, 0);
+        }
+
+        //klik med finger
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Tjekker hvilken retning der køres lige nu, og skifter retning
+            if(Mathf.Sign(rb.velocity.x) == 1)
+            {
+                rb.velocity = new Vector3(-hSpeed, 0);
+            }
+            else
+            {
+                rb.velocity = new Vector2(hSpeed, 0);
+            }
+        }
+	}
+}
---
+++ AAbenHusSpil/Assets/ObstacleGen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGen : MonoBehaviour {
+
+    public float gameSpeed;
+    public List<GameObject> obstacleMarkers;
+    public List<GameObject> ObstaclePrefabs;
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform)
+        {
+            obstacleMarkers.Add(child.gameObject);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject Marker in obstacleMarkers)
+        {
+            Marker.transform.position += Vector3.down * gameSpeed * Time.deltaTime;
+            if(Marker.transform.position.y <= -20)
+            {
+                print("resetter marker");
+                ResetObstacleMarker(Marker);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void ResetObstacleMarker(GameObject Marker)
+    {
+        //Flytter objektet tilbage til toppen
+        Marker.transform.position = new Vector3(0, 20, 0);
+
+        //Sletter det prefab som er på objektet nu
+        foreach (Transform child in Marker.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        //Tilføjer et nyt prefab fra listen
+        Instantiate(ObstaclePrefabs[Random.Range(0,ObstaclePrefabs.Count-1)], Marker.transform);
+    }
+}
